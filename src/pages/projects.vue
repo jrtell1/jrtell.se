592d3f660c9f337f2fde68af9672e093bc965e76
JRTell <john-robin@jrtell.se>
2018-04-01 11:32:19 +0200

    Added media object layout

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -2,13 +2,34 @@
     <section class="section">
         <div class="container">
             <h1 class="title">Projects</h1>
-            <h2 class="subtitle">asdffasdfasdfsad asdasf sadfasf asdfasfasd asdf</h2>
+            <article class="media" v-for="project in projects">
+                <figure class="media-left">
+                    <p class="image is-64x64">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                    </p>
+                </figure>
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong>{{ project.title }}</strong>
+                        </p>
+                    </div>
+                </div>
+            </article>
         </div>
     </section>
 </template>
 
 <script>
   export default {
-    name: 'Projects'
+    name: 'Projects',
+
+    data() {
+      return {
+        projects: [
+          { title: 'AirsoftEvents' }
+        ]
+      }
+    }
   }
 </script>
